f5cfbab5b520452b2bbf3a9ceb27c16653560119
Gene Higgins <ehigginsiii@cnm.edu>
2017-02-07 11:32:05 -0700

    refactored to use FormBuilder

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../signup';
 
 @Component({
@@ -58,15 +58,15 @@ export class SignupFormComponent implements OnInit {
 
     user: FormGroup;
 
-    constructor() {
+    constructor(private fb: FormBuilder) {
     }
 
     ngOnInit() {
-        this.user = new FormGroup({
-            name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-            account: new FormGroup({
-                email: new FormControl('', Validators.required),
-                confirm: new FormControl('', Validators.required)
+        this.user = this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(2)]],
+            account: this.fb.group({
+                email: ['', Validators.required],
+                confirm: ['', Validators.required]
             })
         })
     }
